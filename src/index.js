41ebecf2233f6cfc2305b5a6f9f6e896c2c412e5
maenswirony <meanswirony@gmail.com>
2024-03-12 14:40:56 -0500

    test persist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { jsx as _jsx } from "hono/jsx/jsx-runtime";
 import { serve } from '@hono/node-server';
 import { Button, Frog, TextInput } from 'frog';
-import { app as startSwirl } from './startSwirl';
 import fetch from 'node-fetch';
 // Function to fetch transfer details and extract the username
 const getFname = async (fid) => {
@@ -34,11 +33,19 @@ export const app = new Frog({
     basePath: '/swirl',
     browserLocation: 'http://cultureblocks.world',
     initialState: {
+        castId: "",
         creatorId: 0,
         creatorName: "",
         headline: "",
         emulsifier: "",
-        turns: 6
+        current_turn: 0,
+        turns: 6,
+        responses: [{
+                name: "",
+                message: ""
+            }],
+        synthesis: "",
+        mintLink: ""
     },
     // verify: false
     // Supply a Hub API URL to enable frame verification.
@@ -76,6 +83,7 @@ app.frame('/creator', async (c) => {
     const { frameData, deriveState } = c;
     const state = await deriveState(async (previousState) => {
         if (frameData?.fid && !previousState.creatorName) {
+            previousState.castId = frameData?.castId.hash;
             previousState.creatorId = frameData?.fid;
             previousState.creatorName = await getFname(previousState.creatorId);
         }
@@ -182,7 +190,54 @@ app.frame('/emulsifier', async (c) => {
         ],
     });
 });
-app.route("/startSwirl", startSwirl);
+app.frame('/startSwirl', async (c) => {
+    const { buttonValue, inputText, deriveState } = c;
+    const state = await deriveState(async (previousState) => {
+        // const dataToWrite: SwirlData = { 
+        //   castId: previousState.castId, 
+        //   creatorId: previousState.creatorId, 
+        //   creatorName: previousState.creatorName,
+        //   headline: previousState.headline,
+        //   emulsifier: previousState.emulsifier,
+        //   current_turn: 0,
+        //   turns: 6,
+        //   // responses:[
+        //   //   {
+        //   //     name:previousState.creatorName,
+        //   //     message:
+        //   //   }
+        //   // ] 
+        //   };
+        // saveSwirl(filename, dataToWrite);
+    });
+    const dynamicText = `Everything look good?\n\nSwirl Creator: ${state.creatorName} \nHeadline: ${state.headline}\nEmulsifier: ${state.emulsifier}`;
+    return c.res({
+        image: (_jsx("div", { style: {
+                alignItems: 'center',
+                background: `url('https://i.ibb.co/xhNRrSL/swirl-frame-compressed-compressed-compressed.jpg')`, //TODO create url and pull image from /images
+                backgroundSize: '100% 100%',
+                display: 'flex',
+                flexDirection: 'column',
+                flexWrap: 'nowrap',
+                height: '100%',
+                justifyContent: 'center',
+                textAlign: 'center',
+                width: '100%',
+            }, children: _jsx("div", { style: {
+                    color: 'black',
+                    fontSize: 60,
+                    fontStyle: 'normal',
+                    letterSpacing: '-0.025em',
+                    lineHeight: 1.4,
+                    marginTop: 30,
+                    padding: '0 120px',
+                    whiteSpace: 'pre-wrap',
+                }, children: dynamicText }) })),
+        intents: [
+            _jsx(Button, { action: "/startSwirl", value: "start", children: "Start Swirl" })
+        ],
+    });
+});
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 serve({
